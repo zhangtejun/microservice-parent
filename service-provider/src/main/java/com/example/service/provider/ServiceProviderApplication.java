5ad2e286186351d1415ed70a73c2302dd3739227
zhangtejun <1053946416@qq.com>
2019-03-04 15:06:17 +0800

    zuul

--- service-provider/src/main/java/com/example/service/provider/ServiceProviderApplication.java
+++ service-provider/src/main/java/com/example/service/provider/ServiceProviderApplication.java
@@ -1,4 +1,4 @@
-package com.example.servicep.rovider;
+package com.example.service.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- service-provider/src/main/java/com/example/service/provider/controller/TestController.java
+++ service-provider/src/main/java/com/example/service/provider/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.example.servicep.rovider.controller;
+package com.example.service.provider.controller;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
