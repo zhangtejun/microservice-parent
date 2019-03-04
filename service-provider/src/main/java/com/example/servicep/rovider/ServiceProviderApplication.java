2577fa63aa6c749c28a72da1927d842fa132b565
zhangtejun <1053946416@qq.com>
2019-03-04 15:04:50 +0800

    zuul

--- service-provider/src/main/java/com/example/servicep/rovider/ServiceProviderApplication.java
+++ service-provider/src/main/java/com/example/servicep/rovider/ServiceProviderApplication.java
@@ -3,11 +3,6 @@ package com.example.servicep.rovider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
